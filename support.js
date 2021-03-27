cb48781c7644e0b6d4dbbde92d7e8c6578521d56
HerrListener <herrlistener@gmail.com>
2021-03-27 18:02:00 +0100

    Initial commit

---
+++ support.js
@@ -0,0 +1,46 @@
+registerPlugin({
+    name: 'Support',
+    version: '1.0.0',
+    backends: ['ts3'],
+    description: 'Ein Support Sinusbot Script für MineCookie.de',
+    author: 'leonZ.<leonz@electronu.de>',
+    vars: [{
+        name: "supportchannel",
+        title: "Support Channel",
+        type: "channel"
+    }, {
+        name: "supportmsg",
+        title: "Lege eine Support Naricht für die Supporter fest.",
+        type: "string"
+    }]
+}, (_, config) => {
+   
+    const event = require('event')
+    const engine = require('engine')
+    const backend = require('backend')
+
+    getSupporter = () => {
+        return backend.getClients().filter(client => client.getServerGroups().find(group => group.id() == 29))
+    }
+
+    isSupporter = (client) => {
+        return client.getServerGroups().includes(29)
+    }
+
+    getSupportChannel = () => {
+        return backend.getChannelByID(config.supportchannel)
+    }
+
+    event.on("clientMove", event => {
+        
+        const toChannel = event.toChannel
+
+        if(toChannel && toChannel.id() == config.supportchannel /*&& !isSupporter(event.client)*/) {
+            getSupporter().forEach(client => {
+                client.chat((config.supportmsg.includes("%user%")) ? config.supportmsg.replace("%user%", event.client.name()) : config.supportmsg)
+            })
+        } 
+
+    })
+   
+})
